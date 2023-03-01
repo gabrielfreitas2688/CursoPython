4fdd11c0381273a437433c405305671c0fd19f0c
GABRIEL FREITAS <2018100839@unibrasil.com.br>
2023-03-01 10:32:14 -0300

    Acrescentado o elfi e alterado o Else

--- exercicioif.py
+++ exercicioif.py
@@ -4,4 +4,8 @@ segundo = input("Digite um segundo número: ")
 if primeiro > segundo:
     print(f"O número {primeiro} é maior que {segundo}")
 
-else: print(f"O número {segundo} é maior que {primeiro} ")
+elif segundo > primeiro:
+    print(f"O número {segundo} é maior que {primeiro} ")
+
+else: 
+    print("Os dois números são iguais.")
