b21d87f46619cf2ae7b44238c4a3ffc122cf31bb
GABRIEL FREITAS <2018100839@unibrasil.com.br>
2023-03-01 09:44:42 -0300

    Aula 15, primeiro uso do input

---
+++ aula15.py
@@ -0,0 +1,8 @@
+#Função input - Sempre retorna como string 
+# Conversão para int (forma ruim).
+
+nome = input("Qual seu nome? ")
+idade = int(input("Idade? "))
+data_nascimento = int(input("Data de nascimento? "))
+
+print(f"O seu nome é {nome}, sua idade é {idade} e sua data de nascimento é {data_nascimento}.")
