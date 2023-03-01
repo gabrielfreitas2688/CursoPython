a8b391718ffd5c839b5ffe448b78e424764d9c6c
GABRIEL FREITAS <2018100839@unibrasil.com.br>
2023-03-01 09:56:57 -0300

    Aula 16 - Primeiro uso do if, elif e else

---
+++ aula16.py
@@ -0,0 +1,12 @@
+# if elif else
+
+entrada = input ('Você deseja entrar no sistema? Digite "Sim" ou "Não" ' )
+
+if entrada == "Sim":
+    print("Você entrou!")
+
+elif entrada == "Não":
+    print("Você saiu!")
+
+else:
+    print('Você não digitou "Sim" ou "Não" ')
