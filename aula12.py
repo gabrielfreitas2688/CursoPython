6e914725979e932a9e970e528b697a194015b124
GABRIEL FREITAS <2018100839@unibrasil.com.br>
2023-03-01 01:18:38 -0300

    Exercicio de cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = "Gabriel Freitas"
+idade = 23
+peso = 78
+altura = 1.77
+imc = peso / (altura*altura) 
+
+print("Nome:", nome) 
+print("Idade:", idade)
+print("Peso:", peso)
+print("Altura:", altura)
+print("IMC:", imc)
