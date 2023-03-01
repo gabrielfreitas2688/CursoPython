a9ceb12e34b2449bb5cbd448ec9fbdf1482ee5c5
GABRIEL FREITAS <2018100839@unibrasil.com.br>
2023-03-01 10:30:07 -0300

    Exercício de comparação IF

---
+++ exercicioif.py
@@ -0,0 +1,7 @@
+primeiro = input("Digite um número: ")
+segundo = input("Digite um segundo número: ")
+
+if primeiro > segundo:
+    print(f"O número {primeiro} é maior que {segundo}")
+
+else: print(f"O número {segundo} é maior que {primeiro} ")
