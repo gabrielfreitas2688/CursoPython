9bd7c2e9f8223f2e21018d1bde66c5972965af1d
Gabriel Freitas <gabrielfreitas2688@gmail.com>
2023-03-03 15:17:43 -0300

    Aula 38 - While

---
+++ Modulo1/aula38.py
@@ -0,0 +1,16 @@
+quanti_linhas = 5 
+quanti_colunas = 5
+
+linha = 1 
+coluna = 1
+    
+
+while linha <= quanti_linhas:
+    
+    while coluna <= quanti_colunas:
+        print(coluna,linha, linha, linha, linha)
+    coluna += 1
+linha += 1      
+
+
+
